5cff4be3aa2fe69d086f96832d686a87b659c395
Supriyo Maity <supriyo.codes@gmail.com>
2024-05-04 00:22:06 +0530

    second commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const apikey="86bf9d7970e9de0e1a6993d7cc732171";
+const url="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+let x=document.querySelector("button");
+let y=document.querySelector("input");
+let z=document.querySelector(".weather-icon");
+async function weather(city){
+    const response=await fetch(url+city+`&appid=${apikey}`);
+    
+    if(response.status== 404){
+        
+        document.querySelector(".weather").style.display="none"
+        alert=("Invalid City Name");
+        
+    }
+    else{
+
+        let data= await response.json();
+        console.log(data);
+        document.querySelector(".city").innerText=data.name;
+        document.querySelector(".temp").innerText=data.main.temp+" °C";
+        document.querySelector(".humidity").innerText=data.main.humidity+" %";
+        
+        document.querySelector(".wind-speed").innerText=data.wind.speed+" km/h";
+        if(data.weather[0].main=="Rain"){
+            z.src="rain.png";
+        }
+        else if(data.weather[0].main=="Clouds"){
+            z.src="img/clouds.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            z.src="img/clear.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            z.src="img/drizzle.png";
+        }
+        else if(data.weather[0].main=="Mist"){
+            z.src="img/mist.png";
+        }
+        else if(data.weather[0].main=="Snow"){
+            z.src="img/snow.png";
+        }
+        
+        document.querySelector(".weather").style.display="block";
+    }
+   
+  
+
+}
+x.addEventListener("click",()=>{
+    weather(y.value);
+})
+
+
+
